e35fcfec04581c631f2aa38a203806ae3df3efbd
Martin Saturka <martin.saturka@sourcefabric.org>
2014-02-26 17:34:57 +0100

    Starting support for dynamical setting of Twitter stream params

---
+++ src/lib/twister_main.py
@@ -0,0 +1,351 @@
+#!/usr/bin/env python
+#
+# Citizen Desk
+#
+
+'''
+listen on a port
+spawn and terminate subprocesses on requests
+
+on any request:
+    check if the client is allowed to do requests
+
+on a create POST request:
+* i.e. create a node of provided rank
+
+    params as json:
+        oauth params
+        filter spec
+
+    start a noded
+    write all the params (as json) to the noded
+        http://stackoverflow.com/questions/4585692/python-nonblocking-subprocess-check-stdout/4585898#4585898
+        http://twistedmatrix.com/documents/current/core/howto/process.html
+
+    take PID of the new node, and return it
+
+on a stop POST request:
+* i.e. stop a node of provided PID
+    try to terminate the node
+    try to kill the node
+
+on check GET request:
+    p.poll()
+    os.kill(pid, 0)
+
+'''
+
+import json
+import subprocess
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+from SocketServer import ForkingMixIn#,ThreadingMixIn
+import sys, os, time, atexit, signal
+import logging, logging.handlers
+import resource, urlparse, cgi
+import pwd, grp
+
+NODE_NAME = 'twister_node'
+
+WEB_HOST = 'localhost'
+WEB_PORT = 9054
+
+NODE_PATH = '/opt/citizendesk/bin/twister_node.py'
+SAVE_URL = 'http://localhost:9055/citizendesk/tweets/'
+
+LOG_PATH = '/opt/citizendesk/log/citizendesk/twister_main.log'
+PID_PATH = '/opt/citizendesk/run/twister_main.pid'
+
+ALLOWED_CLIENTS = ['127.0.0.1']
+
+logger = logging.getLogger()
+
+def setup_logger(log_path):
+    global logger
+
+    while logger.handlers:
+        logger.removeHandler(logger.handlers[-1])
+
+    formatter = logging.Formatter("%(asctime)s [%(name)s] %(levelname)s: %(message)s")
+
+    #sh = logging.StreamHandler()
+    #sh.setFormatter(formatter)
+    #logger.addHandler(sh)
+
+    fh = logging.handlers.WatchedFileHandler(log_path)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    logger.setLevel(logging.INFO)
+
+class ConnectParams():
+    def __init__(self):
+        self.web_host = WEB_HOST
+        self.web_port = WEB_PORT
+        self.node_path = NODE_PATH
+        self.save_url = SAVE_URL
+        self.log_path = LOG_PATH
+        self.pid_path = PID_PATH
+        self.allowed = ALLOWED_CLIENTS
+
+    def use_specs(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-h', '--web_host', help='web host of this controller, e.g. ' + str(WEB_HOST), default=WEB_HOST)
+        parser.add_argument('-p', '--web_port', help='web port of this controller, e.g. ' + str(WEB_PORT), type=int, default=WEB_PORT)
+
+        parser.add_argument('-n', '--node_path', help='node path, e.g. ' + str(NODE_PATH))
+        parser.add_argument('-s', '--save_url', help='save url, e.g. ' + str(SAVE_URL))
+
+        parser.add_argument('-l', '--log_path', help='path to log file, e.g. ' + str(LOG_PATH))
+        parser.add_argument('-i', '--pid_path', help='path to pid file, e.g. ' + str(PID_PATH))
+
+        #parser.add_argument('-a', '--allowed_clients', help='ip addresses of allowed clients')
+
+        args = parser.parse_args()
+        if args.web_host:
+            self.web_host = args.web_host
+        if args.web_port:
+            self.web_port = int(args.web_port)
+        if args.node_path:
+            self.node_path = args.node_path
+        if args.save_url:
+            self.save_url = args.save_url
+
+        if args.log_path:
+            self.log_path = args.log_path
+        if args.pid_path:
+            self.pid_path = args.pid_path
+
+        setup_logger(self.log_path)
+
+    def get_web_host():
+        return self.web_host
+
+    def get_web_port():
+        return self.web_port
+
+    def get_node_path():
+        return self.node_path
+
+    def get_save_url():
+        return self.save_url
+
+params = ConnectParams()
+
+class NodeHandler():
+    def __init__(self):
+        self.nodes = {}
+
+    def set_node(pid, process):
+        pid = str(pid)
+        if process is None:
+            if pid in self.nodes:
+                del(self.nodes[pid])
+                return
+        self.nodes[pid] = process
+
+    def get_node(pid):
+        pid = str(pid)
+        if pid not in self.nodes:
+            return None
+        return self.nodes[pid]
+
+nodes = NodeHandler()
+
+class RequestHandler(BaseHTTPRequestHandler):
+
+    def _check_running(self, pid):
+        try:
+            pid = int(pid)
+        except:
+            return False
+
+        try:
+            os.kill(int(pid), 0)
+        except:
+            return False
+
+        return True
+
+    def _write_error(self, msg):
+        self.send_response(404)
+        self.end_headers()
+        self.wfile.write(str(msg) + '\n')
+
+    def _write_json(self, msg):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(str(msg) + '\n')
+
+    def _get_param(self, param, transform):
+        value = None
+
+        form = cgi.FieldStorage(fp=self.rfile, headers=self.headers, environ=environment)
+        for field in form.keys():
+            if field == param:
+                try:
+                    value = form[field]
+                    value = transform(value)
+                except:
+                    value = None
+                    continue
+                if value:
+                    break
+
+        return value
+
+    def do_GET(self):
+        global params
+        global nodes
+
+        if not self.path:
+            self.path = ''
+
+        method = None
+        if self.path.endswith('/_status'):
+            method = 'STATUS'
+
+        if not method:
+            self._write_error('unknown method')
+            return
+
+        pid = self._get_param('pid', lambda x: int(x))
+        if not pid:
+            self._write_error('pid not provided')
+            return
+
+        is_running = self._check_running(pid)
+        if not is_running:
+            old_process = nodes.get_node(pid)
+            if old_process:
+                nodes.set_node(pid, None)
+
+        res = json.dumps({'node': int(pid), 'status': is_running})
+        self._write_json(res)
+
+        return
+
+    def do_POST(self):
+        global params
+        global nodes
+
+        data_string = ''
+        data_struct = None
+
+        if not self.path:
+            self.path = ''
+
+        command = None
+        if self.path.endswith('/_start'):
+            command = 'START'
+        if self.path.endswith('/_stop'):
+            command = 'STOP'
+
+        if not command:
+            self._write_error('unknown command')
+            return
+
+        parsed_path = urlparse.urlparse(self.path)
+        parsed_params = urlparse.parse_qs(parsed_path.query)
+
+        if 'STOP' == command:
+            pid = self._get_param('pid', lambda x: int(x))
+            if not pid:
+                self._write_error('pid not provided')
+                return
+
+            for ind in range(4):
+                try:
+                    os.kill(int(pid), signal.SIGTERM)
+                    time.sleep(0.05)
+                except:
+                    break
+
+            try:
+                os.kill(int(pid), signal.SIGKILL)
+            except:
+                pass
+
+            old_process = nodes.get_node(pid)
+            if old_process:
+                try:
+                    old_process.terminate()
+                except:
+                    pass
+                try:
+                    old_process.kill()
+                except:
+                    pass
+                try:
+                    old_process.wait()
+                except:
+                    pass
+                nodes.set_node(pid, None)
+
+            is_running = self._check_running(pid)
+
+            res = json.dumps({'node': int(pid), 'status': is_running})
+            self._write_json(res)
+
+            return
+
+        if 'START' == command:
+
+            try:
+                data_string = self.rfile.read(int(self.headers['Content-Length']))
+            except:
+                self._write_error('no spec data')
+                return
+
+            try:
+                data_struct = json.loads(data_string.strip())
+            except:
+                self._write_error('can not parse spec data')
+                return
+
+            self.node_path = params.get_node_path()
+            self.exec_params = [NODE_NAME, self.node_path, '-s', params.get_save_url()]
+            twitter_params = data_struct
+
+            pid = None
+            executable_name = 'python' + str(sys.version_info.major) + '.' +str(sys.version_info.minor)
+
+            try:
+                p = subprocess.Popen(self.exec_params, bufsize=1, stdin=subprocess.PIPE, close_fds=TRUE, executable=executable_name)
+                p.stdin.write(json.dump(twitter_params) + '\n')
+                p.stdin.flush()
+                pid = p.pid
+            except:
+                logging.warning('can not write to node')
+                self._write_error('error during node creation')
+                return
+
+            if not pid:
+                self._write_error('can not start node')
+                return
+
+            is_running = self._check_running(pid)
+            if is_running:
+                nodes.set_node(pid, p)
+
+            res = json.dumps({'node': int(pid), 'status': is_running})
+            self._write_json(res)
+
+            return
+
+class DerivedHTTPServer(ForkingMixIn, HTTPServer):
+    pass
+
+def run_server(ip_address, port):
+    global params
+    global logger
+
+    params.use_specs()
+
+    logger.info('starting the controller of Twitter-Streaming nodes web server')
+
+    server_address = (params.get_web_host(), params.get_web_port())
+    httpd = DerivedHTTPServer(server_address, RequestHandler)
+    httpd.serve_forever()
+
+
---
+++ src/lib/twister_node.py
@@ -0,0 +1,451 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys, os, time, json, argparse
+import urllib, select
+import oauth2 as oauth
+import signal, atexit
+import ctypes
+
+from pprint import pformat
+
+from zope.interface import Interface, implements
+
+from twisted.internet import reactor
+from twisted.internet.defer import Deferred, succeed
+from twisted.internet.protocol import Protocol
+from twisted.internet.ssl import ClientContextFactory
+from twisted.web.client import Agent
+from twisted.web.http_headers import Headers
+from twisted.web.iweb import IBodyProducer
+from twisted.python.log import err
+
+try:
+    import setproctitle
+except:
+    if not str(sys.platform).lower().startswith('linux'):
+        sys.stderr.write('either the setproctitle module has to be available, or the OS has to be Linux')
+        sys.exit(1)
+
+SAVE_URL = 'http://localhost:9200/citizendesk/tweets/'
+
+PR_SET_NAME = 15
+NODE_NAME = 'twister_node'
+
+def set_proc_name():
+    name_set = False
+
+    try:
+        setproctitle.setproctitle(NODE_NAME)
+        name_set = True
+    except:
+        name_set = False
+
+    if name_set:
+        return True
+
+    try:
+        libc = ctypes.cdll.LoadLibrary('libc.so.6')
+        buff = ctypes.create_string_buffer(len(NODE_NAME)+1)
+        buff.value = NODE_NAME
+        libc.prctl(PR_SET_NAME, ctypes.byref(buff), 0, 0, 0)
+    except:
+        return False
+
+    return True
+
+class SaveSpecs():
+    def __init__(self):
+        self.specs = {
+            'url': SAVE_URL
+        }
+    def get_specs(self):
+        return self.specs
+
+    def set_specs(self, save_url):
+        self.specs = {
+            'save_url': save_url
+        }
+
+    def use_specs(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-s', '--save_url', help='url for saving the tweets')
+
+        args = parser.parse_args()
+        if args.save_url:
+            self.specs['save_url'] = args.save_url
+
+save_specs = SaveSpecs()
+
+class Params():
+    def get_post_params(self):
+        global stream_filter
+
+        post_params = {}
+        post_params['include_entities'] = 0
+        post_params['stall_warning'] = 'true'
+
+        for one_key in stream_filter:
+            post_params[one_key] = stream_filter[one_key].decode('utf-8')
+
+        return post_params
+
+    def get_oauth_header(self):
+        global oauth_info
+
+        oauth_consumer = oauth.Consumer(key=oauth_info['consumer_key'], secret=oauth_info['consumer_secret'])
+        oauth_token = oauth.Token(key=oauth_info['access_token_key'], secret=oauth_info['access_token_secret'])
+
+        oauth_params = {}
+        oauth_params['oauth_version'] = '1.0'
+        oauth_params['oauth_nonce'] = oauth.generate_nonce()
+        oauth_params['oauth_timestamp'] = int(time.time())
+
+        req_url = 'https://stream.twitter.com/1.1/statuses/filter.json' + '?' + urllib.urlencode(self.get_post_params())
+
+        req = oauth.Request(method='POST', parameters=oauth_params, url=req_url)
+        req.sign_request(oauth.SignatureMethod_HMAC_SHA1(), oauth_consumer, oauth_token)
+        return req.to_header()['Authorization'].encode('utf-8')
+
+    def get_headers(self):
+        conn_headers = {}
+        conn_headers['Host'] = ['stream.twitter.com']
+        conn_headers['Authorization'] = [self.get_oauth_header()]
+        conn_headers['User-Agent'] = ['testing steamer']
+        conn_headers['Content-Type'] = ['application/x-www-form-urlencoded']
+
+        return conn_headers
+
+class StringProducer(object):
+    implements(IBodyProducer)
+ 
+    def __init__(self, body):
+        self.body = body
+        self.length = len(body)
+ 
+    def startProducing(self, consumer):
+        consumer.write(self.body)
+        return succeed(None)
+ 
+    def pauseProducing(self):
+        pass
+ 
+    def stopProducing(self):
+        pass
+
+# Part putting tweets into ES
+
+class ElsClientContextFactory(ClientContextFactory):
+    def getContext(self, hostname, port):
+        return ClientContextFactory.getContext(self)
+
+class ElsResponser(Protocol):
+    def __init__(self, finished):
+        self.finished = finished
+        self.buffer = ''
+        self.count = 0
+
+    def dataReceived(self, data):
+        print('got next data from ES: ' + str(data))
+
+    def connectionLost(self, reason):
+        print 'Finished receiving ES response:', reason.getErrorMessage()
+        self.finished.callback(None)
+
+class TweetSaver(object):
+    def __init__(self):
+        global save_specs
+
+        self.save_url = save_specs['save_url']
+        if not self.save_url.endswith('/'):
+            self.save_url += '/'
+
+    def get_headers(self):
+        host = self.save_url[(self.save_url.find(':')+1):]
+        host = host[:host.find('/')]
+
+        conn_headers = {}
+        conn_headers['Host'] = [host]
+        conn_headers['User-Agent'] = ['twister-node']
+        conn_headers['Content-Type'] = ['application/x-www-form-urlencoded']
+        conn_headers['Accept'] = ['application/json']
+
+        return conn_headers
+
+    def save_tweet(self, message):
+        tweet_id = message.get('id_str')
+        if not tweet_id:
+            return False
+        tweet_data = json.dumps(message)
+
+        contextFactory = ElsClientContextFactory()
+        agent = Agent(reactor, contextFactory)
+
+        send_url = self.base_url + str(tweet_id)
+
+        d_es = agent.request(
+            'POST',
+            send_url,
+            Headers(self.get_headers()),
+            StringProducer(tweet_data))
+
+        borders = ElsResponseBorders()
+        d_es.addCallback(borders.cbRequest)
+        d_es.addBoth(borders.cbShutdown)
+
+        return d_es
+
+class ElsResponseBorders():
+    def cbRequest(self, response):
+
+        print 'Els response version:', response.version
+        print 'Els response code:', response.code
+        print 'Els response phrase:', response.phrase
+        print 'Els response headers:'
+        print pformat(list(response.headers.getAllRawHeaders()))
+        finished = Deferred()
+        response.deliverBody(ElsResponser(finished))
+        return finished
+
+    def cbShutdown(self, ignored):
+        print(ignored)
+        print('shutting down els connection')
+
+# Part taking tweets from Twitter
+
+class TwtClientContextFactory(ClientContextFactory):
+    def getContext(self, hostname, port):
+        return ClientContextFactory.getContext(self)
+
+class TweetProcessor(Protocol):
+    def __init__(self, finished):
+        self.finished = finished
+        self.buffer = ''
+        self.count = 0
+
+    def _process_tweet(self, data):
+
+        self.buffer += data
+        if data.endswith('\r\n') and self.buffer.strip(): # some finished message
+            message = json.loads(self.buffer)
+            self.buffer = ''
+
+            # status messages
+            if message.get('limit'): # error (not a tweet), over the rate limit
+                print('rate limit over, count of missed tweets: ' + str(message['limit'].get('track')))
+            elif message.get('disconnect'): # error (not a tweet), got disconnected
+                print('disconnected: ' + str(message['disconnect'].get('reason')))
+                # should restart the read cycle!
+            elif message.get('warning'): # warning (not a tweet)
+                print 'warning: ' + str(message['warning'].get('message'))
+
+            # actual tweet
+            else:
+                # putting the tweet into elastic search
+                tws = TweetSaver()
+                tws.save_tweet(message)
+
+                # outputting the tweet, development purposes only
+                tid = message.get('id_str')
+                user = message.get('user')
+                uid = str(user['id_str']) if (user and ('id_str' in user)) else ''
+                uname = str(user['name'].encode('utf-8')) if (user and ('name' in user)) else ''
+                ulocation = str(user['location'].encode('utf-8')) if (user and ('location' in user)) else ''
+
+                t_coords = str(message.get('coordinates'))
+                t_geo = str(message.get('geo'))
+                t_place = str(message.get('place'))
+
+                msg_beg = 'https://twitter.com/cdeskdev/status/' + str(tid) + '\n'
+                msg_geo = 'coords: ' + t_coords + ', geo: ' + t_geo + ', place: ' + t_place + '\n'
+                msg_mid = '' + uid + '/' + uname + '/' + ulocation + ': '
+                msg_end = message.get('text').encode('utf-8')
+                print(msg_beg + msg_geo + msg_mid + msg_end)
+                print(str(message.get('entities')) + '\n')
+
+    def dataReceived(self, data):
+
+        self._process_tweet(data)
+
+        #self.count += 1
+        #print('count: ' + str(self.count))
+
+    def connectionLost(self, reason):
+        print 'Finished receiving Twitter stream:', reason.getErrorMessage()
+        self.finished.callback(None)
+
+class TwtResponseBorders():
+    def cbRequest(self, response):
+
+        print 'Twt response version:', response.version
+        print 'Twt response code:', response.code
+        print 'Twt response phrase:', response.phrase
+        print 'Twt response headers:'
+        print pformat(list(response.headers.getAllRawHeaders()))
+        finished = Deferred()
+        response.deliverBody(TweetProcessor(finished))
+        return finished
+
+    def cbShutdown(self, ignored):
+        print(ignored)
+        print('shutting twt down')
+        #reactor.stop()
+
+def make_stream_connection():
+    params = Params()
+    post_data = urllib.urlencode(params.get_post_params())
+
+    contextFactory = TwtClientContextFactory()
+    agent = Agent(reactor, contextFactory)
+
+    d = agent.request(
+        'POST',
+        'https://stream.twitter.com/1.1/statuses/filter.json',
+        Headers(params.get_headers()),
+        StringProducer(post_data))
+
+    borders = TwtResponseBorders()
+    d.addCallback(borders.cbRequest)
+    d.addBoth(borders.cbShutdown)
+
+    return d
+
+# General script passage
+
+def signal_handler(signal_number, frame):
+    global d
+
+    d.cancel()
+    print(os.getpid())
+    #signal.signal(signal.SIGINT, signal_handler)
+    #signal.signal(signal.SIGTERM, process_quit)
+
+    reactor.disconnectAll()
+
+    process_quit(signal_number, frame)
+
+def process_quit(signal_number, frame):
+    cleanup()
+
+def cleanup():
+    print(os.getpid())
+    print('stopping the process')
+    os._exit(0)
+
+oauth_info = {
+    'consumer_key': None,
+    'consumer_secret': None,
+    'access_token_key': None,
+    'access_token_secret': None
+}
+stream_filter = {}
+stream_filter_basic = [
+    'follow',
+    'track',
+    'locations'
+]
+stream_filter_other = [
+    'filter_level',
+    'language'
+]
+
+if __name__ == '__main__':
+
+    if not set_proc_name():
+        sys.exit(1)
+
+    save_specs.use_specs()
+
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, process_quit)
+
+    atexit.register(cleanup)
+
+    twitter_param_list = []
+    while True:
+        rfds, wfds, efds = select.select([sys.stdin], [], [], 1)
+        if rfds:
+            twitter_param_list.append(sys.stdin.readline())
+        else:
+            break
+
+    is_correct = True
+    twitter_params = None
+
+    if not twitter_param_list:
+        is_correct = False
+
+    if is_correct:
+        try:
+            twitter_params = json.loads('\n'.join(twitter_param_list))
+            if type(twitter_params) is not dict:
+                is_correct = False
+            if not twitter_params:
+                is_correct = False
+        except:
+            is_correct = False
+
+    if is_correct:
+        try:
+            if not 'oauth_info' in twitter_params:
+                is_correct = False
+            elif type(twitter_params['oauth_info']) is not dict:
+                is_correct = False
+            #else:
+            #    oauth_info = twitter_params['oauth_info']
+
+            if not 'stream_filter' in twitter_params:
+                is_correct = False
+            elif type(twitter_params['stream_filter']) is not dict:
+                is_correct = False
+            #else:
+            #    stream_filter = twitter_params['stream_filter']
+        except:
+            is_correct = False
+
+    if is_correct:
+        for part in oauth_info:
+            if not part in twitter_params['oauth_info']:
+                is_correct = False
+                break
+            if not twitter_params['oauth_info'][part]:
+                is_correct = False
+                break
+            try:
+                oauth_info[part] = str(twitter_params['oauth_info'][part])
+            except:
+                is_correct = False
+                break
+
+    if is_correct:
+        is_correct = False
+        for part in stream_filter_basic:
+            if part in twitter_params['stream_filter']:
+                if twitter_params['stream_filter'][part]:
+                    try:
+                        stream_filter[part] = str(twitter_params['stream_filter'][part])
+                    except:
+                        is_correct = False
+                        break
+                    if stream_filter[part]:
+                        is_correct = True
+
+    if is_correct:
+        for part in stream_filter_other:
+            if part in twitter_params['stream_filter']:
+                if twitter_params['stream_filter'][part]:
+                    try:
+                        stream_filter[part] = str(twitter_params['stream_filter'][part])
+                    except:
+                        is_correct = False
+                        break
+
+    if is_correct:
+        try:
+            d = make_stream_connection()
+        except:
+            is_correct = False
+
+    if is_correct:
+        reactor.run()
+
