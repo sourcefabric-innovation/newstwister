546d9a0dab59e2fd54fbc5f53f1b7e6b3a506b32
Martin Saturka <martin.saturka@sourcefabric.org>
2014-04-23 11:53:59 +0200

    Search spec parsing moved into CD core

--- opt/newstwister/sbin/newstwisters.py
+++ opt/newstwister/sbin/newstwisters.py
@@ -488,7 +488,7 @@ class QueuedResource(resource.Resource):
     def set_processor(self, processor):
         self.processor = processor
 
-    def _parse_data(self, data):
+    def _extract_data(self, data):
         res = {}
 
         if (not data) or (type(data) is not dict):
@@ -502,121 +502,43 @@ class QueuedResource(resource.Resource):
         res['user_id'] = str(data['user_id'])
         res['request_id'] = str(data['request_id'])
 
-        parsed = {}
         has_parts = False
         if type(data['search_spec']) is not dict:
             return (False, 'unsupported "search_spec" data')
         search_spec = data['search_spec']
+        parsed_spec = {}
 
-        # having_any: boolean, true for OR, false for AND (default)
-        # contains: keywords, #hashtags, @screen_names
-        # since/until: yyyy-mm-dd
-        # radius_unit: "km", "mi"
-        # result_type: "mixed", "recent", "popular"
-        #
         # search_spec = {
-        #    'query': {'having_any': None, 'contains': [], 'from': None, 'to': None, 'without': None, 'since': None, 'until': None},
-        #    'geo': {'latitude': None, 'longitude': None, 'radius': None, 'radius_unit': None},
-        #    'lang': None,
-        #    'count': None,
-        #    'since_id': None,
-        #    'max_id': None,
-        #    'result_type': None
+        #    'q': '',
+        #    'geocode': '',
+        #    'lang': '',
+        #    'count': '',
+        #    'since_id': '',
+        #    'max_id': '',
+        #    'result_type': ''
         # }
 
-        if ('query' not in search_spec) or (type(search_spec['query']) is not dict):
-            return (False, 'query dictionary should be provided in "search_spec" data')
-        query_part = search_spec['query']
-
-        query_connective = ' '
-        if ('having_any' in query_part) and query_part['having_any']:
-            query_connective = ' OR '
-
-        query_terms = []
-
-        if ('contains' in query_part) and (type(query_part['contains']) is list) and query_part['contains']:
-            for one_term in query_part['contains']:
-                one_term = one_term.strip()
-                if not one_term:
-                    continue
-                if one_term.lower() in ['and', 'or']:
-                    return (False, 'boolean operators shall not be term parameters')
-                if ' ' in one_term:
-                    if not one_term.startswith('"'):
-                        one_term = '"' + one_term
-                    if not one_term.endswith('"'):
-                        one_term = one_term + '"'
-                query_terms.append(one_term)
-
-        for spec_key in ['from', 'to']:
-            if (spec_key in query_part) and query_part[spec_key]:
-                screen_name = query_part[spec_key]
-                if screen_name.startswith('@'):
-                    screen_name = screen_name[1:]
-                    if screen_name:
-                        query_terms.append(screen_name)
-
-        if ('without' in query_part) and query_part['without']:
-            query_terms.append('-' + query_part['without'])
-
-        for spec_key in ['since', 'until']:
-            if (spec_key in query_part) and query_part[spec_key]:
-                date_str = query_part[spec_key]
-                if not re.match('^[\d]{4}-[\d]{2}-[\d]{2}$', date_str):
-                    return (False, 'date specifiers shall be in the form "YYYY-MM-DD"')
-                query_terms.append(spec_key + ':' + query_part[spec_key])
-
-        parsed['q'] = query_connective.join(query_terms)
-        if len(query_terms):
-            has_parts = True
-
-        if ('geocode' in search_spec) and (type(search_spec['geocode']) is dict):
-            has_geo = True
-            geo_part = search_spec['geocode']
-            for geo_spec in ['latitude', 'longitude', 'radius', 'radius_unit']:
-                if geo_spec not in geo_part:
-                    has_geo = False
-                    continue
-                if not geo_part[geo_spec]:
-                    has_geo = False
-                    continue
-            if has_geo:
-                if geo_spec['radius_unit'] not in ['km', 'mi']:
-                    has_geo = False
-                    return (False, 'radius unit geo specifier shall be either "km" or "mi"')
-            if has_geo:
-                use_geocode = str(geo_part['latitude']) + ',' + str(geo_part['longitude']) + ','
-                use_geocode += str(geo_part['radius']) + str(geo_part['radius_unit'])
-                parsed['geocode'] = use_geocode
-                has_parts = True
-
-        for one_id_part in ['since_id', 'max_id']:
-            if (one_id_part in search_spec) and search_spec[one_id_part]:
-                if not str(search_spec[one_id_part]).isdigit():
-                    return (False, '"' + str(one_id_part) + '" shall be a digital string')
-                parsed[one_id_part] = str(search_spec[one_id_part])
-                has_parts = True
+        has_parts = False
+        for part in ['q', 'geocode', 'since_id', 'max_id']:
+            if part in search_spec:
+                try:
+                    parsed_spec[part] = str(search_spec[part])
+                except:
+                    return (False, 'can not stringify search criteria')
+                if parsed_spec[part]:
+                    has_parts = True
 
         if not has_parts:
-            return (False, 'no applicable search specification provided')
+            return (False, 'no valid search criteria provided')
 
-        if ('count' in search_spec) and search_spec['count']:
-            if not str(search_spec['count']).isdigit():
-                return (False, '"count" shall be a digital string')
-            parsed['count'] = str(search_spec['count'])
+        for part in ['lang', 'count', 'result_type']:
+            if part in search_spec:
+                try:
+                    parsed_spec[part] = str(search_spec[part])
+                except:
+                    return (False, 'can not stringify auxiliary criteria')
 
-        if ('lang' in search_spec) and search_spec['lang']:
-            if 2 is not len(search_spec['lang']):
-                return (False, '"lang" shall be a (2-letter) language ISO 639-1 specification')
-            parsed['lang'] = str(search_spec['lang'])
-
-        parsed['result_type'] = 'mixed'
-        if ('result_type' in search_spec) and search_spec['result_type']:
-            if search_spec['result_type'] not in ['mixed', 'recent', 'popular']:
-                return (False, '"result_type" shall be one of "mixed", "recent", "popular"')
-            parsed['result_type'] = search_spec['result_type']
-
-        res['search_spec'] = parsed
+        res['search_spec'] = parsed_spec
 
         return (True, res)
 
@@ -629,7 +551,7 @@ class QueuedResource(resource.Resource):
             data = None
 
         try:
-            res = self._parse_data(data)
+            res = self._extract_data(data)
         except:
             res = (False, 'can not parse request data')
 
